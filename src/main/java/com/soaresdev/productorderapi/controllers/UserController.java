c594cf931592c6a551e58c7dec6b2796841577ed
h-Soares <hiagosoares002@gmail.com>
2023-06-13 18:17:45 -0300

    User entity and User controller created.

---
+++ src/main/java/com/soaresdev/productorderapi/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.soaresdev.productorderapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
