862ffff7819905bcd9708d1c43c9855eb6e5b06c
h-Soares <hiagosoares002@gmail.com>
2023-06-13 19:21:11 -0300

    H2 Database.

--- src/main/java/com/soaresdev/productorderapi/entities/User.java
+++ src/main/java/com/soaresdev/productorderapi/entities/User.java
@@ -1,17 +1,30 @@
 package com.soaresdev.productorderapi.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String email;
     private String phone;
+    @Column(nullable = false)
     private String password;
 
     public User() {
