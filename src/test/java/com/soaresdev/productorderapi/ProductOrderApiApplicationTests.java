a207bccfb8c35b643874c13d06c0290b2ac2fedf
h-Soares <hiagosoares002@gmail.com>
2023-06-13 17:56:30 -0300

    Project created.

---
+++ src/test/java/com/soaresdev/productorderapi/ProductOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soaresdev.productorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
